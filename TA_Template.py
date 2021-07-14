e6d8f79bccf61cbb52aa21204d08da4f2fa063a2
SanjuktaBhatt <64887786+SanjuktaBhatt@users.noreply.github.com>
2021-07-15 01:42:28 +0530

    Create TA_Template.py

---
+++ TA_Template.py
@@ -0,0 +1,26 @@
+#Acitivity 1
+#Import pygame
+
+#Initialize pygame
+ 
+#Create a screen
+
+#Set a caption to the screen
+
+#Begin the game loop
+
+
+
+
+
+    #Activity 2
+    #Load the background image
+   
+    #Scale the background image
+    
+    #Display background image on screen
+    
+    #Update the display
+    
+    
+#Quit the game outside the loop   
