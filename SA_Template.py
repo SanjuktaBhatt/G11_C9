8ba4654871a6528680f572745335c06caaea9679
SanjuktaBhatt <64887786+SanjuktaBhatt@users.noreply.github.com>
2021-08-15 22:22:09 +0530

    Update SA_Template.py

--- SA_Template.py
+++ SA_Template.py
@@ -1,3 +1,14 @@
+#Important Links
+#Backgrond Image:
+#https://github.com/SanjuktaBhatt/G11_C9/blob/main/BJFC-G7-Revision-C11_Road.png
+#Yellow Car:
+#https://github.com/SanjuktaBhatt/G11_C9/blob/main/VA3G02SLC005_car%20_Slide41_V1-02(2).png
+#Red Car:
+#https://github.com/SanjuktaBhatt/G11_C9/blob/main/Asset%201.png
+#Stone:
+#https://github.com/SanjuktaBhatt/G11_C9/blob/main/Stone-02.png
+
+
 import pygame
 pygame.init() 
 
